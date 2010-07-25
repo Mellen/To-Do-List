597ff5696df104963b1e11f75713690aa9add41b
Matthew Ellen <matt@matthewellen.co.uk>
2010-07-25 01:26:07 +0100

    Added in a context menu to allow for deltion and editing of items. Separated the todo list and items into separate classes. Created and edit window.
    Signed-off-by: Matthew Ellen <matt@matthewellen.co.uk>

--- ToDoList/MainWindow.xaml.cs
+++ ToDoList/MainWindow.xaml.cs
@@ -15,67 +15,43 @@ using System.Xml.Linq;
 using System.Configuration;
 using System.IO;
 
-namespace ToDoList
+namespace ToDoListApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        XDocument ToDoList;
+        ToDoList tdl = new ToDoList();
         public MainWindow()
         {
             InitializeComponent();
-            AppSettingsReader asr = new AppSettingsReader();
-            string listFileName = (string)asr.GetValue("listFileName", typeof(string));
-            if (File.Exists(listFileName))
-            {
-                ToDoList = XDocument.Load(listFileName);
-                LoadItems();
-            }
-            else
-            {
-                ToDoList = new XDocument();
-                ToDoList.Add(new XElement("todolist"));
-            }
-        }
-
-        private void LoadItems()
-        {
-            lvToDo.Items.Clear();
-            foreach (XElement item in ToDoList.Root.Descendants("todo").Where(x => x.Descendants("donedatetime").Count() == 0))
-            {
-                lvToDo.Items.Add(item.Element("description").Value);
-            }
+            DataContext = tdl;
         }
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
             if (!txtItemDesc.Text.Trim().Equals(string.Empty))
             {
-                XElement item = new XElement("todo");
-                XElement createAt = new XElement("createdatetime");
-                createAt.Value = DateTime.Now.ToString("yyyy-dd-MMThh:mm:ss.ms");
-                item.Add(createAt);
-                XElement desc = new XElement("description");
-                desc.Value = txtItemDesc.Text.Trim();
-                item.Add(desc);
-                ToDoList.Root.Add(item);
-                lvToDo.Items.Add(item.Element("description").Value);
-                txtItemDesc.Text = "";
+                tdl.Additem(txtItemDesc.Text.Trim());
             }
-        }
-
-        private bool notdone(XElement x)
-        {
-            return (x.Descendants("donedatetime") == null);
+            txtItemDesc.Text = "";
+            lvToDo.Items.Refresh();
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            AppSettingsReader asr = new AppSettingsReader();
-            string listFileName = (string)asr.GetValue("listFileName", typeof(string));
-            ToDoList.Save(listFileName);
+            tdl.Save();
+        }
+
+        private void MarkAsDone(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult done = MessageBox.Show("Mark this as done?", "Done?", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
+            if (done == MessageBoxResult.Yes)
+            {
+                (lvToDo.SelectedItem as ToDoItem).DoneDateTime = DateTime.Now.ToString("yyyy-dd-MMThh:mm:ss.ms");
+            }
+            lvToDo.Items.Refresh();
         }
 
         private void lvToDo_MouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -83,11 +59,25 @@ namespace ToDoList
             MessageBoxResult done = MessageBox.Show("Mark this as done?", "Done?", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
             if (done == MessageBoxResult.Yes)
             {
-                XElement item = ToDoList.Root.Descendants("todo").Where(x => x.Descendants("donedatetime").Count() == 0).ElementAt(lvToDo.SelectedIndex);
-                XElement doneAt = new XElement("donedatetime", DateTime.Now.ToString("yyyy-dd-MMThh:mm:ss.ms"));
-                item.Add(doneAt);
-                LoadItems();
+                (lvToDo.SelectedItem as ToDoItem).DoneDateTime = DateTime.Now.ToString("yyyy-dd-MMThh:mm:ss.ms");
             }
+            lvToDo.Items.Refresh();
+        }
+
+        private void EditMenu_Click(object sender, RoutedEventArgs e)
+        {
+            EditPopup ep = new EditPopup(lvToDo.SelectedItem as ToDoItem);
+            ep.ShowDialog();
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult del = MessageBox.Show("Delete this item?", "Delete?", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
+            if (del == MessageBoxResult.Yes)
+            {
+                tdl.DeleteItem(lvToDo.SelectedItem as ToDoItem);
+            }
+            lvToDo.Items.Refresh();
         }
     }
 }
---
+++ ToDoList/ToDoList.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Configuration;
+using System.IO;
+using System.ComponentModel;
+
+namespace ToDoListApp
+{
+    [Serializable]
+    public class ToDoItem : INotifyPropertyChanged
+    {
+        private XElement todo;
+        internal XElement ToDo { get { return todo; } }
+        internal ToDoItem(XElement todo)
+        {
+            this.todo = todo;
+        }
+        public string CreatedDateTime
+        {
+            get { return todo.Element("createdatetime").Value; }
+            set
+            {
+                todo.Element("createdatetime").Value = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("CreatedDateTime"));
+                }
+            }
+        }
+        public string Description
+        {
+            get { return todo.Element("description").Value; }
+            set
+            {
+                todo.Element("description").Value = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Description"));
+                }
+            }
+        }
+
+        public string DoneDateTime
+        {
+            get { return todo.Element("donedatetime") == null ? "" : todo.Element("donedatetime").Value; }
+            set
+            {
+                if (todo.Element("donedatetime") == null)
+                {
+                    todo.Add(new XElement("donedatetime"));
+                }
+                
+                todo.Element("donedatetime").Value = value;
+
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("DoneDateTime"));
+                }
+            }
+        }
+
+        public bool IsDone { get { return (todo.Element("donedatetime") != null); } }
+
+        public override string ToString()
+        {
+            return Description;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    [Serializable]
+    public class ToDoList: INotifyPropertyChanged
+    {
+        [NonSerialized]
+        private XDocument todoList;
+
+        public List<ToDoItem> Items { get; private set; }
+
+        public ToDoList()
+        {
+            AppSettingsReader asr = new AppSettingsReader();
+            string listFileName = (string)asr.GetValue("listFileName", typeof(string));
+            if (File.Exists(listFileName))
+            {
+                todoList = XDocument.Load(listFileName);
+                Items = (from e in todoList.Root.Elements()
+                         select new ToDoItem(e)).ToList();
+            }
+            else
+            {
+                todoList = new XDocument();
+                todoList.Add(new XElement("todolist"));
+                Items = new List<ToDoItem>();
+            }
+
+        }
+
+        public void Additem(string description)
+        {
+            XElement item = new XElement("todo");
+            XElement createAt = new XElement("createdatetime");
+            createAt.Value = DateTime.Now.ToString("yyyy-dd-MMThh:mm:ss.ms");
+            item.Add(createAt);
+            XElement desc = new XElement("description");
+            desc.Value = description;
+            item.Add(desc);
+
+            todoList.Root.Add(item);
+            Items.Add(new ToDoItem(item));
+
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("Items"));
+            }
+        }
+
+        public void DeleteItem(ToDoItem item)
+        {
+            Items.Remove(item);
+            item.ToDo.Remove();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void Save()
+        {
+            AppSettingsReader asr = new AppSettingsReader();
+            string listFileName = (string)asr.GetValue("listFileName", typeof(string));
+            todoList.Save(listFileName);
+        }
+    }
+}
